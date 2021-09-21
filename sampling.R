c7d25f50aad1d0c653a986c81c309feca7baace5
= <=>
2021-09-21 13:35:43 -0400

    add files

---
+++ sampling.R
@@ -0,0 +1,94 @@
+library(arrangements)
+library(invgamma)
+C = function(j, r, h, Upaj,Upajj){
+  s = 2*rep(h[j],length(Upajj))
+  Upajm1 = solve(Upaj)
+  term = function(nu){
+    out = (-1)^(sum(abs(nu)))
+    for(k in 1:length(nu)){
+      out = out*choose(s[k],nu[k])
+    }
+    fp = t(s/2 - nu)%*%Upajm1%*%(s/2-nu)
+    sp = t(s/2-nu)%*%Upajm1%*%Upajj
+    denom = factorial(r)*factorial((sum(abs(s))-2*r))
+    out  = out * (fp^r * sp^(sum(abs(s))-2*r) / denom)
+    return(out)
+  }
+  out2 = 0
+  perm = permutations(x = 0:s[1],k=s[1],replace=TRUE)
+  for(i in 1:length(perm[,1])){
+    out2 = out2 + term(perm[i,])
+  }
+  return(out2[1,1])
+}
+
+sampDmarginal = function(j,Ujj,Upaj,Upajj,h,alpha){
+  s = 2*rep(h[j],length(Upajj))
+  maxr = floor(sum(abs(s))/2)
+  probs = c()
+  shapes = c()
+  Upajm1 = solve(Upaj)
+  mj = length(s)
+  for(k in 1:(maxr+1)){
+    r = k-1
+    probs[k] = C(j,r,h,Upaj,Upajj)
+    shapes[k] = -1 - (r + mj/2 - alpha[j]/2)
+  }
+  betaj = c((1/2)*(Ujj - t(Upajj)%*%Upajm1%*%Upajj))
+  probs = probs/sum(probs)
+  rchoice = sample(0:maxr,size = 1,prob = probs)
+  #print(shapes[rchoice])
+  samp = rinvgamma(1,shapes[rchoice],rate = betaj)
+  return(samp)
+}
+
+#sample from f(x) = (x+a)^k * standardnormalpdf(x)
+aMin = -10
+aMax = 10
+aStep = .1
+xMin = -5
+xMax = 5
+xStep = .1
+sampF = function(k,a){
+  u = runif(1,0,1)
+  path = paste("./table/tablekequals",k,".csv",sep="")
+  line = read.csv(file = path,
+                  header = TRUE, 
+                  nrows = 1,
+                  check.names = FALSE,
+                  row.names = 1,
+                  skip = as.integer((a-aMin)/aStep))
+  u = .542
+  leftendpoint = findInterval(u,line)
+  rightendpoint = leftendpoint + 1
+  left_xval = xMin + xStep*(leftendpoint - 1)
+  right_xval = xMin + xStep*leftendpoint
+  left_Fval = if(leftendpoint != 0) line[[leftendpoint]] else 0
+  right_Fval = if(rightendpoint != (length(line)+1)) line[[rightendpoint]] else 1
+  slope = (right_Fval-left_Fval)/(right_xval-left_xval)
+  approx_inv = if(slope!=0) left_xval + (u - left_Fval)/slope else left_xval
+  return approx_inv
+}
+
+pp = sampF(4,.3)
+
+
+Upajj = c(0, 1)
+Upaj = matrix(c(2,0,0,2),nrow = 2)
+h = matrix(c(1,0,0,1),nrow = 2)
+alpha = 10*matrix(c(1,1,1,1),nrow = 2)
+
+
+C(1,1,h,Upaj,Upajj)
+samps = c()
+for(i in 1:1000){
+  samps[i] = sampDmarginal(1,10,Upaj,Upajj,h,alpha)
+}
+hist(samps,breaks = 100)
+mean(samps)
+mode(samps)
+
+# - a - 1 = pow -> -a = pow + 1 -> a = - pow - 1
+
+
+
---
+++ tabulate_LgivenD_CDF.py
@@ -0,0 +1,61 @@
+from scipy import special
+import numpy as np
+import os
+import shutil
+import pandas as pd
+
+#I_j is integral from -inf to t of x^j e^(-1/2 x^2)
+def gen_I(t,max_j = 10):
+    if(t == np.inf):
+        Ijm2 = np.sqrt(2*np.pi)
+        Ijm1 = 0       
+    else:
+        Ijm2 = np.sqrt(np.pi/2)*(special.erf(t/np.sqrt(2))+1)
+        Ijm1 = -np.exp(-t**2/2)
+    yield Ijm2
+    yield Ijm1
+    j = 2
+    while(j<=max_j):
+        Ij = (0 if t == np.inf else -t**(j-1)) * np.exp(-t**2/2) + (j-1)*Ijm2
+        yield Ij
+        j = j + 1
+        Ijm2 = Ijm1
+        Ijm1 = Ij
+
+
+#need to integrate from 0 to t of (x+a)^k e^(-x^2/2) where k is even
+#its a binomial expansion
+#sum_{i=1}^k (k choose i)  a^(k-i) integral 0 to t of x^i e^(-x^2/2) dx
+#sum_{i=1}^k (k choose i) a^(k-i) IncompleteGamma(i+1,t)
+
+def F(a,k):
+    norm = sum(special.binom(k,j)*a**(k-j)*I_j
+                   for j,I_j in enumerate(gen_I(np.inf,max_j = k)))
+    def outFunction(t):
+        return sum(special.binom(k,j)*a**(k-j)*I_j
+                   for j,I_j in enumerate(gen_I(t,max_j = k)))/norm
+    return outFunction
+
+def tabulateF(kMax = 20,
+              aStep = .01,
+              aMin = -10,
+              aMax = 10,
+              xMin = -5,
+              xMax = 5,
+              xStep = .01):
+    dirname = "table"
+    shutil.rmtree("table") if os.path.isdir(dirname) else 0
+    os.mkdir(dirname)
+    for k in range(2,kMax+2,2):
+        filepath = dirname + "/tablekequals" + str(k) + ".csv"
+        table = []
+        a_list = []
+        for a in np.arange(aMin,aMax + aStep,aStep):
+            a_list = a_list + ["a="+str(np.round(a,5))]
+            table.append([np.round(F(a,k)(x),5) for x in np.arange(xMin,xMax + xStep,xStep)])
+            x_list = ["x="+str(np.round(x,5)) for x in np.arange(xMin,xMax + xStep,xStep)]
+        pd.DataFrame(data = table,
+                     index = a_list,
+                     columns = x_list).to_csv(filepath,header = True,index = True)
+
+tabulateF(kMax = 6,aStep = .1,xStep = .1)
